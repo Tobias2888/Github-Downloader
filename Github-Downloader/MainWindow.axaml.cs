46ab6feb1dbdaf236714a8af5deba321b02170b2
Tobias2888 <iclub.hofinger@gmail.com>
2026-01-24 18:46:36 +0100

    First working Version

--- Github-Downloader/MainWindow.axaml.cs
+++ Github-Downloader/MainWindow.axaml.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Net.Http;
 using System.Text.Json;
@@ -17,6 +18,7 @@ public partial class MainWindow : Window
     private List<Repo> _repos;
     private string _appdataPath;
     private string _cachePath;
+    private string _reposConfigFilePath;
     private string _token = "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";
     
     public MainWindow() => InitializeComponent();
@@ -25,11 +27,26 @@ public partial class MainWindow : Window
     {
         _appdataPath = Path.Join(DirectoryHelper.GetAppDataDirPath(), "github-downloader");
         _cachePath = Path.Join(DirectoryHelper.GetCacheDirPath(), "github-downloader");
+        _reposConfigFilePath = Path.Join(_appdataPath, "repos.json");
         DirectoryHelper.CreateDir(_appdataPath);
         DirectoryHelper.CreateDir(_cachePath);
-        FileHelper.Create(Path.Join(_appdataPath, "repos.json"));
         
-        _repos = new List<Repo>();
+        
+
+        if (File.Exists(_reposConfigFilePath))
+        {
+            string jsonString = File.ReadAllText(_reposConfigFilePath);
+            _repos = JsonSerializer.Deserialize<List<Repo>>(jsonString);
+        }
+        else
+        {
+            _repos = new List<Repo>();
+        }
+        
+        foreach (var repo in _repos)
+        {
+            CreateTrackedRepoEntry(repo.Name);
+        }
     }
 
     private async void BtnAddRepo_OnClick(object? sender, RoutedEventArgs e)
@@ -71,14 +88,31 @@ public partial class MainWindow : Window
         {
             Url = url,
             DownloadUrl = response.assets[0].url,
+            Name = response.name
         });
 
-        CreateTrackedRepoEntry(response);
+        SaveRepos();
+        CreateTrackedRepoEntry(response.name);
 
         TbxUrl.Text = "";
     }
+    
+    private void SaveRepos()
+    {
+        if (!File.Exists(_reposConfigFilePath))
+        {
+            FileHelper.Create(_reposConfigFilePath);
+        }
 
-    private void CreateTrackedRepoEntry(Response response)
+        string jsonString = JsonSerializer.Serialize(_repos, new JsonSerializerOptions
+        {
+            WriteIndented = true
+        });
+        
+        File.WriteAllText(_reposConfigFilePath, jsonString);
+    }
+
+    private void CreateTrackedRepoEntry(string name)
     {
         StackPanel stackPanel = new();
         stackPanel.Orientation = Orientation.Horizontal;
@@ -86,18 +120,23 @@ public partial class MainWindow : Window
         Button btnUninstall = new();
         btnUninstall.Content = "Uninstall";
         btnUninstall.Background = Brushes.Red;
+
+        Button btnUpdate = new();
+        btnUpdate.Content = "Update";
         
         TextBlock textBlock = new();
-        textBlock.Text = response.name;
+        textBlock.Text = name;
         
         stackPanel.Children.Add(textBlock);
         stackPanel.Children.Add(btnUninstall);
+        stackPanel.Children.Add(btnUpdate);
         
         TrackedRepos.Children.Add(stackPanel);
     }
 
     private async void BtnUpdateAll_OnClick(object? sender, RoutedEventArgs e)
     {
+        List<string> debPaths = new();
         foreach (Repo repo in _repos)
         {
             HttpResponseMessage httpResponse = await Api.GetRequest(repo.Url, _token);
@@ -112,6 +151,47 @@ public partial class MainWindow : Window
             string downloadUrl = response.assets[0].url;
 
             await Api.DownloadFileAsync(repo.DownloadUrl, Path.Join(_cachePath, response.assets[0].name), _token);
+            
+            debPaths.Add(Path.Join(_cachePath, response.assets[0].name));
         }
+        InstallDebs(debPaths);
+    }
+
+    public static void InstallDebs(List<string> debPaths)
+    {
+        string installCommand = "pkexec apt install -y ";
+        foreach (string debPath in debPaths)
+        {
+            installCommand += $"\"{debPath}\" ";
+        }
+
+        Console.WriteLine(installCommand);
+        
+        var process = new Process
+        {
+            StartInfo = new ProcessStartInfo
+            {
+                FileName = "pkexec",
+                Arguments = installCommand,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            }
+        };
+
+        process.Start();
+
+        string output = process.StandardOutput.ReadToEnd();
+        string error = process.StandardError.ReadToEnd();
+
+        process.WaitForExit();
+
+        if (process.ExitCode != 0)
+        {
+            throw new Exception($"Installation failed:\n{error}");
+        }
+
+        Console.WriteLine(output);
     }
 }
--- Github-Downloader/Repo.cs
+++ Github-Downloader/Repo.cs
@@ -4,5 +4,6 @@ public class Repo
 {
     public required string Url { get; set; }
     public required string DownloadUrl { get; set; }
+    public required string Name { get; set; }
     public string CurrentName { get; set; } = "";
 }
